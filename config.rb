5be1b81bdaba88b9a5385cf3ec0d4d16143bb618
huos3203 <724987481@qq.com>
2014-05-28 20:01:57 +0800

    _config.yml冒号“:”后面不能跟空格 否则：could not find expected ':' while scanning a simple key at line 102 column 1 问题2：You have already activated rake 10.3.2, but your Gemfile requires rake 0.9.2.2. Prepending `bundle exec` to your command may solve this. 方法:直接修改改Gemfile的内容，将rake版本号改为‘~>10.3.2’ 然后， rake install rake setup_github_pages rake deploy

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/MyOctopress/"
+http_images_path = "/MyOctopress/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/MyOctopress/fonts"
+css_dir = "public/MyOctopress/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
